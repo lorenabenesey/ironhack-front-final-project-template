9acd3f06e83848c2e4411e4487a61a0e68cbb7b0
Lorena Benesey <lorena.benesey@gmail.com>
2022-12-20 19:46:18 +0100

    anon key

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_ENV),
   routes,
 });
+
+// Change document titles
+// Route guard for auth routes
 
 export default router;
