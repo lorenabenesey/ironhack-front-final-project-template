66c158636d446e2b15bf7b558f21fb1308ee86be
Lorena Benesey <lorena.benesey@gmail.com>
2023-01-26 20:21:11 +0100

    Icons

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,32 +4,44 @@
     <ul class="task-list">
       <div v-for="task in taskStore.tasks">
         <li v-if="!task.is_complete" class="flex items-center mb-4">
-          <input @click="isCompleted(task.id)" type="checkbox"
-            class="form-checkbox h-6 w-6 text-indigo-600 transition duration-150 ease-in-out" />
-          <span v-if="!task.is_complete" class="ml-3 flex-1 font-medium text-gray-900">{{ task.title }}</span>
-          
-          <EditModal :id="task.id"/>
-          <button @click="changeTask(task.id)"
-            class="text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition duration-150 ease-in-out">
-            Edit
-          </button>
-          <button @click="deleteTask(task.id)"
-            class="text-sm font-medium text-red-600 hover:text-red-500 focus:outline-none focus:underline transition duration-150 ease-in-out ml-2">
-            Delete
-          </button>
-          <button @click="isCompleted(task.id)"
-            class="text-sm font-medium text-green-600 hover:text-green-500 focus:outline-none focus:underline transition duration-150 ease-in-out ml-2">
-            Mark as Completed
-          </button>
+          <input
+            @click="isCompleted(task.id)"
+            type="checkbox"
+            class="form-checkbox h-6 w-6 text-indigo-600 transition duration-150 ease-in-out"
+          />
+          <span
+            v-if="!task.is_complete"
+            class="ml-3 flex-1 font-medium text-gray-900"
+            >{{ task.title }}</span
+          >
+
+          <EditModal :id="task.id" />
+
+          <div>
+            <font-awesome-icon
+              :icon="['fas', 'trash']"
+              @click="deleteTask(task.id)"
+              class="mr-5"
+            />
+            <font-awesome-icon
+              :icon="['fas', 'edit']"
+              @click="changeTask(task.id)"
+            />
+          </div>
         </li>
       </div>
     </ul>
     <form @submit.prevent="addTask" class="flex items-center mt-4">
-      <input v-model="newTaskText" type="text"
+      <input
+        v-model="newTaskText"
+        type="text"
         class="form-input w-2/5 py-2 px-3 rounded-md border text-gray-900 leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-        placeholder="Add a new task" />
-      <button type="submit"
-        class="ml-4 py-2 px-3 rounded-md text-sm font-medium leading-5 text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:border-gray-700 focus:shadow-outline-indigo active:bg-gray-800">
+        placeholder="Add a new task"
+      />
+      <button
+        type="submit"
+        class="ml-4 py-2 px-3 rounded-md text-sm font-medium leading-5 text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:border-gray-700 focus:shadow-outline-indigo active:bg-gray-800"
+      >
         Add Task
       </button>
     </form>
@@ -41,14 +53,18 @@
           <span class="ml-3 flex-1 font-medium text-gray-400 line-through">{{
             task.title
           }}</span>
-          <button @click="deleteTask(task.id)"
-            class="text-sm font-medium text-red-600 hover:text-red-500 focus:outline-none focus:underline transition duration-150 ease-in-out ml-2">
-            Delete
-          </button>
-          <button @click="isNotCompleted(task.id)"
-            class="text-sm font-medium text-green-600 hover:text-green-500 focus:outline-none focus:underline transition duration-150 ease-in-out ml-2">
-            Mark as Uncompleted
-          </button>
+          <div>
+            <font-awesome-icon
+              :icon="['fas', 'trash']"
+              @click="deleteTask(task.id)"
+              class="mr-5"
+            />
+            <font-awesome-icon
+              :icon="['fas', 'plus']"
+              @click="isNotCompleted(task.id)"
+              class="mr-5"
+            />
+          </div>
         </li>
       </div>
     </ul>
@@ -62,6 +78,12 @@ import { useUserStore } from "../store/user";
 import { onMounted } from "vue";
 import EditModal from "../components/EditModal.vue";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faEdit, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faTrash, faEdit, faPlus);
+
 const userStore = useUserStore();
 const newTaskText = ref("");
 const errorMsg = ref(null);
@@ -69,7 +91,6 @@ const taskStore = useTaskStore();
 const tasks = ref(null);
 const task = ref(null);
 const promptMessage = ref(null);
-
 
 async function addTask() {
   try {
@@ -98,7 +119,6 @@ async function deleteTask(id) {
   }
 }
 
-
 async function changeTask(id) {
   try {
     promptMessage.value = await prompt("Edit your task: ");
@@ -112,7 +132,6 @@ async function changeTask(id) {
     console.log(errorMsg.value);
   }
 }
-
 
 async function isCompleted(id) {
   try {
